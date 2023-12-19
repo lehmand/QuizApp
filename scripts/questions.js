6e78551f2b02af0fd978423ba950b87270ee32f1
Daniel Lehmann <daniel.lehmann188@gmail.com>
2023-12-19 01:54:42 +0100

    updated script

---
+++ scripts/questions.js
@@ -0,0 +1,99 @@
+let htmlQuestions = [
+    {
+        'question': 'What does HTML stand for?',
+        'answer1': 'Hyper Text Markup Language',
+        'answer2': 'Hyperlinks and Text Markup Language',
+        'answer3': 'Home Tool Markup Language',
+        'right_answer': 1
+    },
+    {
+        'question': 'Who is making the Web standards?',
+        'answer1': 'Mozilla',
+        'answer2': 'Microsoft',
+        'answer3': 'The World Wide Web Consortium',
+        'right_answer': 3
+    },
+    {
+        'question': 'Choose the correct HTML element for the largest heading',
+        'answer1': '<h1>',
+        'answer2': '<h6>',
+        'answer3': '<head>',
+        'right_answer': 1
+    },
+    {
+        'question': 'Choose the correct HTML element to define a important text',
+        'answer1': '<b>',
+        'answer2': '<strong>',
+        'answer3': '<i>',
+        'right_answer': 2
+    },
+];
+
+let cssQuestions = [
+    {
+        'question': 'What does CSS stand for?',
+        'answer1': 'Cascading Style Sheet',
+        'answer2': 'Creative Style Sheet',
+        'answer3': 'Computer Style Sheet',
+        'right_answer': 1
+    },
+
+    {
+        'question': 'What is the correct CSS syntax?',
+        'answer1': '{body:color=black;}',
+        'answer2': '{body;color:black;}',
+        'answer3': 'body {color: black;}',
+        'answer4': 'body:color=black;',
+        'right_answer': 3
+    },
+
+    {
+        'question': 'Which property is used to change the background color?',
+        'answer1': 'background-color',
+        'answer2': 'bgcolor',
+        'answer3': 'color',
+        'right_answer': 1
+    },
+
+    {
+        'question': 'Which CSS property is used to change the text color of an element?',
+        'answer1': 'text-color',
+        'answer2': 'color',
+        'answer3': 'fgcolor',
+        'right_answer': 2
+    },
+];
+
+let javascriptQuestions = [
+    {
+        'question': 'Inside which HTML element do we put the JavaScript?',
+        'answer1': '<script>',
+        'answer2': '<scripting>',
+        'answer3': '<javascript>',
+        'answer4': '<jv>',
+        'right_answer': 1
+    },
+
+    {
+        'question': 'Where is the correct place to insert a JavaScript?',
+        'answer1': 'The <body> section',
+        'answer2': 'The <head> section',
+        'answer3': 'Both the <head> section and the <body> section are correct',
+        'right_answer': 3
+    },
+
+    {
+        'question': 'The external JavaScript file must contain the <script> tag.',
+        'answer1': 'True',
+        'answer2': 'False',
+        'right_answer': 2
+    },
+
+    {
+        'question': 'How do you create a function in JavaScript?',
+        'answer1': 'function myFunction()',
+        'answer2': 'function:myFunction()',
+        'answer3': 'function = myFunction()',
+        'right_answer': 1
+    },
+];
