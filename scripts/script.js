eb4ac2c01aaaf605b9c0835ad89cffdeb7cfd61b
Daniel Lehmann <daniel.lehmann188@gmail.com>
2023-12-21 02:23:32 +0100

    added button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,35 @@
 let currentIndex = 0;
-// let questionHTML = htmlQuestions[currentIndex];
 let questionCSS = cssQuestions[currentIndex];
 let questionJavascript = javascriptQuestions[currentIndex];
-let firstQuestion = currentIndex + 1;
+
 
 function loadHTML(){
     let content = document.getElementById('quizbox');
-    content.innerHTML = '';
     content.innerHTML = htmlTemplate();
+}
+
+function loadCSS(){
+
+}
+
+function loadJavascript(){
+    
 }
 
 function nextQuestion(){
     currentIndex++;
     loadHTML();
+}
+
+function checkAnswer(option){
+    let optionNum = Number(option.slice(-1));
+    let correctAnswer = `answer${htmlQuestions[currentIndex].right_answer}`;
+    
+    if(optionNum == htmlQuestions[currentIndex].right_answer){
+        document.getElementById(option).classList.add('bg-success');
+    } else {
+        document.getElementById(option).classList.add('bg-danger');
+        document.getElementById(correctAnswer).classList.add('bg-success');
+    }
+    document.getElementById('nextButton').disabled = false;
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,34 +1,42 @@
 function htmlTemplate(){
     let questionHTML = htmlQuestions[currentIndex];
+    let currentQuestion = currentIndex + 1;
     return /*html*/`
             <div class="card">
                 <img src="./assets/img/questionmark.jpg" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">${questionHTML.question}</h5>
     
-                    <div class="card mb-3">
-                        <div class="card-body">
+                    <div class="card mb-3 answerOption">
+                        <div class="card-body" id="answer1" onclick="checkAnswer('answer1')">
                             <span class="answerOption">${questionHTML.answer1}</span>
                         </div>
                     </div>
     
-                    <div class="card mb-3">
-                        <div class="card-body">
+                    <div class="card mb-3 answerOption">
+                        <div class="card-body" id="answer2" onclick="checkAnswer('answer2')">
                             <span class="answerOption">${questionHTML.answer2}</span>
                         </div>
                     </div>
     
-                    <div class="card mb-3">
-                        <div class="card-body">
+                    <div class="card mb-3 answerOption">
+                        <div class="card-body" id="answer3" onclick="checkAnswer('answer3')">
                             <span class="answerOption">${questionHTML.answer3}</span>
                         </div>
                     </div>
                     
                     <div class="card-bottom">
-                        <span><b>${firstQuestion}</b> of <b>${htmlQuestions.length}</b> Questions</span>
-                        <button class="btn btn-primary" onclick="nextQuestion()">Next Question</button>
+                        <span><b>${currentQuestion}</b> of <b>${htmlQuestions.length}</b> Questions</span>
+                        <button class="btn btn-primary" onclick="nextQuestion()" id="nextButton" disabled>Next Question</button>
                     </div>
                 </div>
             </div>
+    `
+}
+
+function welcome(){
+    return /*html*/`
+        <h1 class="welcomeText">Viel Spass beim Quizzen</h1>
+          <span>Welches Quiz möchtest du spielen?</span>
     `
 }
